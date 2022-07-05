560a829a168c167a94e011f51bf25e5161cd7b78
yunsohee <s2128@e-mirim.hs.kr>
2022-07-05 22:17:01 +0900

    코드 줄이기

--- Rock Paper Scissors/Rock Paper Scissors.cpp
+++ Rock Paper Scissors/Rock Paper Scissors.cpp
@@ -8,41 +8,38 @@ int main(void)
 
 	srand(time(NULL));
 
+	int value = 0;
+
 	while (1)
 	{
 		printf("가위(0), 바위(1), 보(2) 중 선택하세요. >>");
 		scanf_s("%d", &player);
 		com = rand() % 3;
 
-		//플레이어가 가위일때 0
-		if (player == 0)
-		{
-			if (com == 0) result = 0;
-			else {
-				if (com == 1) result = -1;
-				else result = 1;
-			}
-		}
+		value = player - com;
 
-		//플레이어가 바위일때 1
-		if (player == 1)
-		{
-			if (com == 0) result = 1;
-			else {
-				if (com == 1) result = 0;
-				else result = -1;
-			}
-		}
+		//플레이어가 가위일때 0	가위 0 = 0 무
+		//						바위 1 = -1 패
+		//						보 2  = -2 승	승
+		
+		//플레이어가 바위일때 1	가위 0 = 1 승	승
+		//						바위 1 = 0 무
+		//						보 2  = -1 패
+		 
+		//플레이어가 보일때 2		가위 0 = 2 패
+		//						바위 1 = 1 승	승
+		//						보 2  = 0 무
 
-		//플레이어가 보일때 2
-		if (player == 2)
-		{
-			if (com == 0) result = -1;
-			else {
-				if (com == 1) result = 1;
-				else result = 0;
-			}
-		}
+
+		//무의 조건 : 0
+		value = result;
+
+		//승의 조건 : 1, -2
+		if (value == 1 || value == -2) result = 1;
+
+		//패의 조건 : -1, 2
+		if (value == -1 || value == 2) result = -1;
+		
 		printf("player : %d vs com : %d = %d \n", player, com, result);
 	}
 
