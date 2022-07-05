11f624dbfe6c24031a0a21ae8492ed4eeeb998df
yunsohee <s2128@e-mirim.hs.kr>
2022-07-05 22:24:18 +0900

    코드 줄이기2

--- Rock Paper Scissors/Rock Paper Scissors.cpp
+++ Rock Paper Scissors/Rock Paper Scissors.cpp
@@ -2,9 +2,21 @@
 #include <stdlib.h>
 #include <time.h>
 
+int g_result[3][3] =
+{
+	//	가위0, 바위1, 보2 com
+	//	가위 0
+	{0,-1,1},
+	//	바위 1
+	{1,0,-1},
+	//	보 2
+	{-1,1,0}
+};
+
 int main(void) 
 {
 	int player, com, result = 0;
+	char szResult[3][10] = { "패","무","승" };
 
 	srand(time(NULL));
 
@@ -30,17 +42,8 @@ int main(void)
 		//						바위 1 = 1 승	승
 		//						보 2  = 0 무
 
-
-		//무의 조건 : 0
-		value = result;
-
-		//승의 조건 : 1, -2
-		if (value == 1 || value == -2) result = 1;
-
-		//패의 조건 : -1, 2
-		if (value == -1 || value == 2) result = -1;
-		
-		printf("player : %d vs com : %d = %d \n", player, com, result);
+		result = g_result[player][com];
+		printf("player : %d vs com : %d = %s \n", player, com, szResult[result+1]);
 	}
 
 }
